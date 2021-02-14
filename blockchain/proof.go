71b139ce9232433eed1f640190c4d2d1c8ad9bde
lleonesouza <lleonesouza@live.com>
2021-02-14 16:09:59 -0300

    Fix the Run() func

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -10,16 +10,16 @@ import (
 	"math/big"
 )
 
+// Difficulty of the proof of work
+const Difficulty = 15
+
 // ProofOfWork struct
 type ProofOfWork struct {
 	Block  *Block
 	Target *big.Int
 }
 
-// Difficulty of the proof of work
-const Difficulty = 12
-
-// NewProof create a new proof
+// NewProof create a new proof of work
 func NewProof(b *Block) *ProofOfWork {
 	target := big.NewInt(1)
 	target.Lsh(target, uint(256-Difficulty))
@@ -35,6 +35,8 @@ func (pow *ProofOfWork) InitData(nonce int) []byte {
 		[][]byte{
 			pow.Block.PrevHash,
 			pow.Block.Data,
+			ToHex(int64(nonce)),
+			ToHex(int64(Difficulty)),
 		},
 		[]byte{},
 	)
@@ -42,39 +44,19 @@ func (pow *ProofOfWork) InitData(nonce int) []byte {
 	return data
 }
 
-// ToHex to transform a num into Hex Bytes
-func ToHex(num int64) []byte {
-	buff := new(bytes.Buffer)
-	err := binary.Write(buff, binary.BigEndian, num)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	return buff.Bytes()
-}
-
-// Validate the proof of work
-func (pow *ProofOfWork) Validate() bool {
-	var intHash big.Int
-	data := pow.InitData(pow.Block.Nonce)
-
-	hash := sha256.Sum256(data)
-	intHash.SetBytes(hash[:])
-
-	return intHash.Cmp(pow.Target) == -1
-}
-
 // Run the proof of work
 func (pow *ProofOfWork) Run() (int, []byte) {
 	var intHash big.Int
 	var hash [32]byte
+
 	nonce := 0
 
 	for nonce < math.MaxInt64 {
 		data := pow.InitData(nonce)
-		hash := sha256.Sum256(data)
+		hash = sha256.Sum256(data)
 
 		fmt.Printf("\r%x", hash)
+
 		intHash.SetBytes(hash[:])
 
 		if intHash.Cmp(pow.Target) == -1 {
@@ -88,4 +70,27 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 	}
 
 	return nonce, hash[:]
+}
+
+// Validate the proof of work
+func (pow *ProofOfWork) Validate() bool {
+	var intHash big.Int
+
+	data := pow.InitData(pow.Block.Nonce)
+
+	hash := sha256.Sum256(data)
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(pow.Target) == -1
+}
+
+// ToHex to transform a num into Hex Bytes
+func ToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return buff.Bytes()
 }
