a194052c37c3d5fd31a4a7770018e9eea2868360
lleonesouza <lleonesouza@live.com>
2021-02-14 16:12:26 -0300

    Add proof of work validation on main()

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/lleonesouza/blockchain/blockchain"
 )
@@ -11,11 +12,16 @@ func main() {
 
 	chain.AddBlock("First Block after Genesis")
 	chain.AddBlock("Secibd Block after Genesis")
-
+	fmt.Println()
+	fmt.Println()
 	for _, block := range chain.Blocks {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %x\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+
 		fmt.Println("")
 	}
 
